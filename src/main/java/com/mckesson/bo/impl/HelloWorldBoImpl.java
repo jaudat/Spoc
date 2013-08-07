dd8f71ccdd0479f446f026b4781fb9ecf36780fc
zakmysta <zakmysta@gmail.com>
2013-08-07 12:05:08 -0400

    fix web service to handle empty name parameter

--- src/main/java/com/mckesson/bo/impl/HelloWorldBoImpl.java
+++ src/main/java/com/mckesson/bo/impl/HelloWorldBoImpl.java
@@ -5,7 +5,7 @@ import com.mckesson.bo.HelloWorldBo;
 public class HelloWorldBoImpl implements HelloWorldBo {
 	
 	public String getHelloWorld(String name) {
-		if (name == null)
+		if (name == null || name.isEmpty())
 			return "Hello World!";
 		return "Hello " + name + "!";
 	}
