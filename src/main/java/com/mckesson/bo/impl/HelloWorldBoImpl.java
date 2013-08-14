8158265cd08564fc3c6177eb127b23f2f7472c23
zakmysta <zakmysta@gmail.com>
2013-08-14 09:56:17 -0400

    add mckesson logo to printed pdf

--- src/main/java/com/mckesson/bo/impl/HelloWorldBoImpl.java
+++ src/main/java/com/mckesson/bo/impl/HelloWorldBoImpl.java
@@ -1,7 +1,10 @@
 package com.mckesson.bo.impl;
 
 import java.awt.print.PrinterException;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import org.apache.pdfbox.exceptions.COSVisitorException;
 import org.apache.pdfbox.pdmodel.PDDocument;
@@ -9,6 +12,7 @@ import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.edit.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.font.PDFont;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.graphics.xobject.PDJpeg;
 
 import com.mckesson.bo.HelloWorldBo;
 
@@ -29,12 +33,20 @@ public class HelloWorldBoImpl implements HelloWorldBo {
 			PDPage page = new PDPage();
 			document.addPage( page );
 			PDFont font = PDType1Font.HELVETICA_BOLD;
-			PDPageContentStream contentStream = new PDPageContentStream(document, page);
+			InputStream in = new FileInputStream(new File(System.getProperty("user.dir") + "/mckesson.jpg"));
+			PDJpeg img = new PDJpeg(document, in);
+			PDPageContentStream contentStream = new PDPageContentStream(document, page, true, false);
 			
+			contentStream.drawImage(img, 100, 700);
 			contentStream.beginText();
 			contentStream.setFont( font, 12 );
-			contentStream.moveTextPositionByAmount( 100, 700 );
+			contentStream.moveTextPositionByAmount( 100, 600 );
 			contentStream.drawString( "Here is the text I recieved from the client: " + name);
+			contentStream.endText();
+			contentStream.beginText();
+			contentStream.setFont( font, 12 );
+			contentStream.moveTextPositionByAmount( 100, 500 );
+			contentStream.drawString( "Second sentence printed: " + name);
 			contentStream.endText();
 			
 			contentStream.close();
@@ -62,12 +74,20 @@ public class HelloWorldBoImpl implements HelloWorldBo {
 			PDPage page = new PDPage();
 			document.addPage( page );
 			PDFont font = PDType1Font.HELVETICA_BOLD;
-			PDPageContentStream contentStream = new PDPageContentStream(document, page);
+			InputStream in = new FileInputStream(new File(System.getProperty("user.dir") + "/mckesson.jpg"));
+			PDJpeg img = new PDJpeg(document, in);
+			PDPageContentStream contentStream = new PDPageContentStream(document, page, true, false);
 			
+			contentStream.drawImage(img, 100, 700);
 			contentStream.beginText();
 			contentStream.setFont( font, 12 );
-			contentStream.moveTextPositionByAmount( 100, 700 );
+			contentStream.moveTextPositionByAmount( 100, 600 );
 			contentStream.drawString( "Here is the text I recieved from the client: " + name);
+			contentStream.endText();
+			contentStream.beginText();
+			contentStream.setFont( font, 12 );
+			contentStream.moveTextPositionByAmount( 100, 500 );
+			contentStream.drawString( "Second sentence printed: " + name);
 			contentStream.endText();
 			
 			contentStream.close();
